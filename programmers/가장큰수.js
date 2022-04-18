29f775adbe466cb58b792d2bde4120f8ef6ee5fe
firely95 <90025832+jjb7134@users.noreply.github.com>
2022-04-18 18:08:04 +0900

    고득점kit 정렬문제풀이

--- programmers/가장큰수.js
+++ programmers/가장큰수.js
@@ -1,5 +1,9 @@
-function solution(numbers){
-    let answer=numbers.map(c => c+'').sort((a,b)=> (b+a)-(a+b)).join('');
-    // 배열의 숫자들을 문자로 바꿔주고 내림차순으로 정렬해주고 문자열로 병합
+function solution(numbers) {
+    let answer = numbers.map(c => c + '').sort((a, b) => (b + a) - (a + b)).join('');
+    // 배열의 숫자들을 문자로 바꿔주기
+    // 순서대로 조합(b+a)과 역순조합(a+b)를 비교정렬 
+    // 문자열로 병합
+
+    //배열이 0이면 0 아니면 최대값을 반환
     return answer;
 }
