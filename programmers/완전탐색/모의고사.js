11ed04125c4ecdea6f201fa43b3e4ece7c841bd0
firely95 <90025832+jjb7134@users.noreply.github.com>
2022-04-19 18:59:58 +0900

    완전탐색 문제풀이

--- programmers/완전탐색/모의고사.js
+++ programmers/완전탐색/모의고사.js
@@ -21,3 +21,24 @@ function solution(answers) {
 }
 let answers=[1,2,3,4,5];
 console.log(solution(answers));
+
+// ===========================================//
+
+function solution(answers) {
+    let answer = [];
+    let a1 = [1, 2, 3, 4, 5];
+    let a2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    let a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+    let a1c = answers.filter((e,i)=> e === a1[i%a1.length]).length;
+    let a2c = answers.filter((e,i)=> e === a2[i%a2.length]).length;
+    let a3c = answers.filter((e,i)=> e === a3[i%a3.length]).length;
+    let max = Math.max(a1c,a2c,a3c);
+
+    if (a1c === max) {answer.push(1)};
+    if (a2c === max) {answer.push(2)};
+    if (a3c === max) {answer.push(3)};
+
+
+    return answer;
+}
---
+++ programmers/완전탐색/소수찾기.js
@@ -0,0 +1,32 @@
+function isPrime(n) { //소수 판별 함수
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) return false;
+    }
+    return true;
+}
+
+function dfs(set, arr, fixed) {
+    if (arr.length >= 1) { //요소가 적어도 1개이상일때
+        for (let i = 0; i < arr.length; i++) {
+            let newFixed = fixed + arr[i]; //문자열로 감싸줌
+            let copyArr = [...arr];  // 그 문자열을 배열안에 담음
+            copyArr.splice(i, 1) //배열로 각각 쪼개줌
+
+            if (isPrime(parseInt(newFixed))) { //조합된 수가 소수면 담아주자
+                set.add(parseInt(newFixed));
+            }
+            dfs(set, copyArr, newFixed);
+        }
+    }
+}
+
+
+function solution(numbers) {
+    let nums = numbers.split(''); //가독성 좋게 미리 선언해주자
+    let set = new Set(); //중복된 값 제외하고 담는 메서드
+
+    dfs(set, nums, ''); //순열함수로 수를 조합하자
+
+    return set.size;
+}
